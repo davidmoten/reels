4581873ee6ceee8eb5bcce7155b46148b868dfcd
Dave Moten <davidmoten@gmail.com>
2022-09-09 13:56:02 +1000

    dev

--- src/main/java/com/github/davidmoten/reels/internal/SupervisorDefault.java
+++ src/main/java/com/github/davidmoten/reels/internal/SupervisorDefault.java
@@ -4,18 +4,20 @@ import com.github.davidmoten.reels.ActorRef;
 import com.github.davidmoten.reels.Context;
 import com.github.davidmoten.reels.Supervisor;
 
-public final class SupervisorDefault implements Supervisor{
+public final class SupervisorDefault implements Supervisor {
 
     public static final SupervisorDefault INSTANCE = new SupervisorDefault();
-    
+
     private SupervisorDefault() {
         // prevent instantiation
     }
-    
+
     @Override
     public void processFailure(Context context, ActorRef<?> actorRef, Throwable error) {
         error.printStackTrace();
         actorRef.dispose();
+        System.out.println(
+                "actor.onMessage threw and was caught by SupervisorDefault. The actor was disposed and will not process further messages.");
     }
 
 }
