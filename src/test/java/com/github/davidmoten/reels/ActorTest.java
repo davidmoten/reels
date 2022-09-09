a58c5d14a6640f2d30af668a2a0607b8861cd528
Dave Moten <davidmoten@gmail.com>
2022-09-09 13:06:51 +1000

    dev

--- src/test/java/com/github/davidmoten/reels/ActorTest.java
+++ src/test/java/com/github/davidmoten/reels/ActorTest.java
@@ -14,7 +14,9 @@ public class ActorTest {
             @Override
             public void processFailure(Context context, ActorRef<?> actorRef, Throwable error) {
                 error.printStackTrace();
-            }};
+                actorRef.dispose();
+            }
+        };
         ActorRef<Object> a = c //
                 .messageClass(Object.class) //
                 .scheduler(Scheduler.computation()) //
@@ -24,9 +26,34 @@ public class ActorTest {
                 }) //
                 .match(String.class, (ctxt, s) -> {
                     ctxt.self().tell(2);
-                    latch.countDown();                    
-                })
-                .build();
+                    latch.countDown();
+                }).build();
+        a.tell(123);
+        latch.await();
+    }
+    
+    @Test
+    public void testTyped() throws InterruptedException {
+        CountDownLatch latch = new CountDownLatch(3);
+        Context c = new Context();
+        Supervisor supervisor = new Supervisor() {
+            @Override
+            public void processFailure(Context context, ActorRef<?> actorRef, Throwable error) {
+                error.printStackTrace();
+                actorRef.dispose();
+            }
+        };
+        ActorRef<Number> a = c //
+                .messageClass(Number.class) //
+                .scheduler(Scheduler.computation()) //
+                .supervisor(supervisor) //
+                .match(Integer.class, (ctxt, n) -> {
+                    ctxt.self().tell((Double) 1.2);
+                }) //
+                .match(Double.class, (ctxt, s) -> {
+                    ctxt.self().tell(2);
+                    latch.countDown();
+                }).build();
         a.tell(123);
         latch.await();
     }
