5942b3583398503a9f0260aa86f52ac508203196
Dave Moten <davidmoten@gmail.com>
2022-09-15 13:36:01 +1000

    dev

--- src/main/java/com/github/davidmoten/reels/SupervisedActorRef.java
+++ src/main/java/com/github/davidmoten/reels/SupervisedActorRef.java
@@ -1,9 +1,16 @@
 package com.github.davidmoten.reels;
 
-public interface SupervisedActorRef<T> extends ActorRef<T>{
+public interface SupervisedActorRef<T> extends ActorRef<T> {
 
+    /**
+     * Recreates the Actor object that processes messages. The message queue is
+     * untouched.
+     */
     void restart();
-    
+
+    /**
+     * Clears the queue of messages waiting to be processed by the Actor.
+     */
     void clearQueue();
-    
+
 }
--- src/main/java/com/github/davidmoten/reels/Supervisor.java
+++ src/main/java/com/github/davidmoten/reels/Supervisor.java
@@ -3,13 +3,19 @@ package com.github.davidmoten.reels;
 public interface Supervisor {
 
     /**
-     * Processes a thrown error from Actor.onMessage. This method <b>must not throw</b>.
+     * Processes a thrown error from Actor.onMessage. This method <b>must not
+     * throw</b>.
      * 
      * <p>
      * Note that because the method is provided with the actorRef that the dispose
      * method can be called on the actorRef which will stop all further processing
      * of messages including already queued ones. If the actorRef is not disposed
      * then message processing will continue with following messages as normal.
+     * 
+     * <p>
+     * A {@link SupervisedActorRef} also has methods to restart the actor (recreate
+     * the actor object which discards current state in the actor) but retain the
+     * queued messages, and also the ability to clear the current message queue.
      * 
      * @param context  context the actor
      * @param actorRef reference to the actor where the error occurred
