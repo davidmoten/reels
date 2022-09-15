ffa72880206196af6fbb8f78cc00ce3e79ab8931
Dave Moten <davidmoten@gmail.com>
2022-09-15 10:40:03 +1000

    dev

--- src/main/java/com/github/davidmoten/reels/Disposable.java
+++ src/main/java/com/github/davidmoten/reels/Disposable.java
@@ -2,21 +2,23 @@ package com.github.davidmoten.reels;
 
 public interface Disposable {
 
-    static Disposable DISPOSED = new Disposable() {
+    void dispose();
+    
+    boolean isDisposed();
+    
+    static Disposable DISPOSED = new Disposed();
+    
+    static final class Disposed implements Disposable {
 
         @Override
         public void dispose() {
-            // no-op
+            // do nothing
         }
 
         @Override
         public boolean isDisposed() {
             return true;
         }
-    };
-
-    void dispose();
-    
-    boolean isDisposed();
+    }
 
 }
