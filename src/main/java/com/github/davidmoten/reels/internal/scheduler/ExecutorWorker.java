083ec56f5a01fc0730edd8a2841abebfd53424a6
Dave Moten <davidmoten@gmail.com>
2022-09-15 10:08:05 +1000

    add dodgy scheduler

---
+++ src/main/java/com/github/davidmoten/reels/internal/scheduler/ExecutorWorker.java
@@ -0,0 +1,53 @@
+package com.github.davidmoten.reels.internal.scheduler;
+
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.TimeUnit;
+
+import com.github.davidmoten.reels.Disposable;
+import com.github.davidmoten.reels.Worker;
+
+final class ExecutorWorker implements Worker {
+    private volatile boolean disposed;
+    private final ScheduledExecutorService executor;
+
+    ExecutorWorker(ScheduledExecutorService executor) {
+        this.executor = executor;
+    }
+
+    @Override
+    public void dispose() {
+        disposed = true;
+    }
+
+    @Override
+    public boolean isDisposed() {
+        return disposed;
+    }
+
+    @Override
+    public Disposable schedule(Runnable run) {
+        if (disposed) {
+            return Disposable.DISPOSED;
+        } else {
+            return new FutureTask(executor.submit(run));
+        }
+    }
+
+    @Override
+    public Disposable schedule(Runnable run, long delay, TimeUnit unit) {
+        if (disposed) {
+            return Disposable.DISPOSED;
+        } else {
+            return new FutureTask(executor.schedule(run, delay, unit));
+        }
+    }
+
+    @Override
+    public Disposable schedulePeriodically(Runnable run, long initialDelay, long period, TimeUnit unit) {
+        if (disposed) {
+            return Disposable.DISPOSED;
+        } else {
+            return new FutureTask(executor.scheduleAtFixedRate(run, initialDelay, period, unit));
+        }
+    }
+}
