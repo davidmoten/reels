ff82e0c25b1cedbca6d056b3fdf8b5ccc094d633
Dave Moten <davidmoten@gmail.com>
2022-09-15 22:30:13 +1000

    remove unused method

--- src/main/java/com/github/davidmoten/reels/internal/Exceptions.java
+++ src/main/java/com/github/davidmoten/reels/internal/Exceptions.java
@@ -23,25 +23,6 @@ public final class Exceptions {
     private Exceptions() {
         throw new IllegalStateException("No instances!");
     }
-    /**
-     * Convenience method to throw a {@code RuntimeException} and {@code Error} directly
-     * or wrap any other exception type into a {@code RuntimeException}.
-     * @param t the exception to throw directly or wrapped
-     * @return because {@code propagate} itself throws an exception or error, this is a sort of phantom return
-     *         value; {@code propagate} does not actually return anything
-     */
-    
-    public static RuntimeException propagate( Throwable t) {
-        /*
-         * The return type of RuntimeException is a trick for code to be like this:
-         *
-         * throw Exceptions.propagate(e);
-         *
-         * Even though nothing will return and throw via that 'throw', it allows the code to look like it
-         * so it's easy to read and understand that it will always result in a throw.
-         */
-        throw wrapOrThrow(t);
-    }
     
     /**
      * If the provided Throwable is an Error this method
