a2042315b05e2782e6ccd0a1012aaa637a7a9ddb
Dave Moten <davidmoten@gmail.com>
2022-09-21 19:55:06 +1000

    javadoc

--- src/main/java/com/github/davidmoten/reels/Supervisor.java
+++ src/main/java/com/github/davidmoten/reels/Supervisor.java
@@ -6,11 +6,18 @@ import com.github.davidmoten.reels.internal.supervisor.SupervisorDefault;
 public interface Supervisor {
 
     /**
-     * Processes a thrown error from Actor.onMessage. This method <b>must not
-     * throw</b>.
+     * Processes an error thrown by {@link Actor#onMessage(MessageContext, T)} or an
+     * error thrown by {@link Actor#onStop(MessageContext)}.
      * 
      * <p>
-     * Note that because the method is provided with the actorRef that the dispose
+     * An error thrown by the {@link Actor#onStop(MessageContext)} will be wrapped
+     * in an {@link OnStopException}.
+     * 
+     * <P>
+     * This method <b>must not throw</b>.
+     * 
+     * <p>
+     * Note that because the method is provided with the actorRef, the dispose
      * method can be called on the actorRef which will stop all further processing
      * of messages including already queued ones. If the actorRef is not disposed
      * then message processing will continue with subsequent messages as normal.
@@ -20,14 +27,16 @@ public interface Supervisor {
      * the actor object which discards current state in the actor) but retain the
      * queued messages, and also the ability to clear the current message queue.
      * 
+     * 
      * @param context  context the actor
      * @param actorRef reference to the actor where the error occurred
-     * @param error    the error throw in the Actor.onMessage method
+     * @param error    the error throw in the Actor.onMessage method. Wrapped in
+     *                 OnStopException if thrown by onStop method.
      */
     void processFailure(Context context, SupervisedActorRef<?> actorRef, Throwable error);
 
     static Supervisor defaultSupervisor() {
         return SupervisorDefault.INSTANCE;
     }
-    
+
 }
