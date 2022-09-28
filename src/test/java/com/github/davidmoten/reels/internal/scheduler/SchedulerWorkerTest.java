fd5e5b0940157d44e552690b9152d514bbeb5734
Dave Moten <davidmoten@gmail.com>
2022-09-28 20:20:13 +1000

    add tests

---
+++ src/test/java/com/github/davidmoten/reels/internal/scheduler/SchedulerWorkerTest.java
@@ -0,0 +1,12 @@
+package com.github.davidmoten.reels.internal.scheduler;
+
+import org.junit.Test;
+
+public class SchedulerWorkerTest {
+
+    @Test
+    public void test() {
+        // TODO
+    }
+
+}
