3c7d3ef15b4b8d812a28ebc145cb09e8cbeb27bd
Dave Moten <davidmoten@gmail.com>
2022-09-28 20:34:01 +1000

    add tests

--- src/test/java/com/github/davidmoten/reels/internal/scheduler/SchedulerWorkerTest.java
+++ src/test/java/com/github/davidmoten/reels/internal/scheduler/SchedulerWorkerTest.java
@@ -1,12 +1,45 @@
 package com.github.davidmoten.reels.internal.scheduler;
 
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.atomic.AtomicBoolean;
+
 import org.junit.Test;
+import org.mockito.Mockito;
+
+import com.github.davidmoten.reels.Disposable;
+import com.github.davidmoten.reels.Worker;
 
 public class SchedulerWorkerTest {
 
     @Test
     public void test() {
-        // TODO
+        Worker w = Mockito.mock(Worker.class);
+        SchedulerWorker s = new SchedulerWorker(w);
+        assertFalse(s.isDisposed());
+        s.dispose();
+        assertTrue(s.isDisposed());
+        // is disposed so will not schedule
+        assertTrue(Disposable.disposed() == s.schedule(() -> {
+        }));
+        assertTrue(Disposable.disposed() == s.schedule(() -> {
+        }, 10, TimeUnit.MILLISECONDS));
+        assertTrue(Disposable.disposed() == s.schedulePeriodically(() -> {
+        }, 1, 10, TimeUnit.MILLISECONDS));
+    }
+
+    @Test
+    public void testSchedule() {
+        Worker w = Mockito.mock(Worker.class);
+        Runnable r = () -> {};
+        Mockito.when(w.schedule(r)).thenReturn(Disposable.disposed());
+        SchedulerWorker s = new SchedulerWorker(w);
+        s.schedule(r);
+        Mockito.verify(w, Mockito.times(1)).schedule(r);
+        Mockito.verifyNoMoreInteractions(w);
+        
     }
 
 }
