2b1cc669eaa7d676b5ecaad54ef306be403b9d4d
Dave Moten <davidmoten@gmail.com>
2022-09-30 21:07:56 +1000

    dev

---
+++ src/test/java/com/github/davidmoten/reels/internal/scheduler/SplitResponsibilityScheduledExecutorServiceTest.java
@@ -0,0 +1,13 @@
+package com.github.davidmoten.reels.internal.scheduler;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class SplitResponsibilityScheduledExecutorServiceTest {
+
+    @Test
+    public void test() {
+    }
+
+}
