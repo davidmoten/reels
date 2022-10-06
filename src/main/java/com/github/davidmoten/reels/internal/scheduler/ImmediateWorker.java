8fb76bdf64e2cd2efd3dfde6dbb7383629379c75
Dave Moten <davidmoten@gmail.com>
2022-10-06 21:45:30 +1100

    use CanSchedule interface

--- src/main/java/com/github/davidmoten/reels/internal/scheduler/ImmediateWorker.java
+++ src/main/java/com/github/davidmoten/reels/internal/scheduler/ImmediateWorker.java
@@ -5,7 +5,7 @@ import java.util.concurrent.TimeUnit;
 import com.github.davidmoten.reels.Disposable;
 import com.github.davidmoten.reels.Worker;
 
-public final class ImmediateWorker implements Worker {
+public final class ImmediateWorker extends AbstractCanScheduleDisposable implements Worker {
 
     private volatile boolean disposed;
 
@@ -15,35 +15,31 @@ public final class ImmediateWorker implements Worker {
     }
 
     @Override
-    public Disposable schedule(Runnable run) {
-        if (!disposed) {
-            run.run();
-        }
-        return Disposable.disposed();
-    }
-
-    @Override
-    public Disposable schedule(Runnable run, long delay, TimeUnit unit) {
-        if (!disposed) {
-            try {
-                unit.sleep(delay);
-            } catch (InterruptedException e) {
-                return Disposable.disposed();
-            }
-        }
-        if (!disposed) {
-            run.run();
-        }
-        return Disposable.disposed();
-    }
-
-    @Override
-    public Disposable schedulePeriodically(Runnable run, long initialDelay, long period, TimeUnit unit) {
-        throw new UnsupportedOperationException("cannot schedule periodically with the immediate scheduler");
-    }
-
-    @Override
     public boolean isDisposed() {
         return disposed;
+    }
+    
+    @Override
+    public Disposable _schedule(Runnable run) {
+        run.run();
+        return Disposable.disposed();
+    }
+
+    @Override
+    public Disposable _schedule(Runnable run, long delay, TimeUnit unit) {
+        try {
+            unit.sleep(delay);
+        } catch (InterruptedException e) {
+            return Disposable.disposed();
+        }
+        if (!disposed) {
+            run.run();
+        }
+        return Disposable.disposed();
+    }
+
+    @Override
+    public Disposable _schedulePeriodically(Runnable run, long initialDelay, long period, TimeUnit unit) {
+        throw new UnsupportedOperationException("cannot schedule periodically with the immediate scheduler");
     }
 }
