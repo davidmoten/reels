4f5f76a52f5c45da6dfaf88cfc0ef526f54a6158
Dave Moten <davidmoten@gmail.com>
2022-10-10 09:24:40 +1100

    dev

--- src/main/java/com/github/davidmoten/reels/Actor.java
+++ src/main/java/com/github/davidmoten/reels/Actor.java
@@ -3,6 +3,14 @@ package com.github.davidmoten.reels;
 public interface Actor<T> {
 
     /**
+     * Called just after creating this (either initially or on supervisor initiated
+     * restart), before processing any messages.
+     * 
+     * @param self ActorRef referring to this
+     */
+    void preStart(ActorRef<T> self);
+    
+    /**
      * Processes a queued message to the actor. If this method throws then the
      * Supervisor allocated to the Actor will handle it (default is
      * {@code Supervisor.defaultSupervisor()}.
@@ -10,14 +18,6 @@ public interface Actor<T> {
      * @param message message to be processed
      */
     void onMessage(Message<T> message);
-
-    /**
-     * Called just after creating this (either initially or on supervisor initiated
-     * restart), before processing any messages.
-     * 
-     * @param self ActorRef referring to this
-     */
-    void preStart(ActorRef<T> self);
 
     /**
      * This method called after an actor is stopped (via {@link ActorRef#stop()} or
