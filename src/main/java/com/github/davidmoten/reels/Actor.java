d8b386f3e44bbedb7751267e4e819e100db4e794
Dave Moten <davidmoten@gmail.com>
2022-10-09 12:14:34 +1100

    javadoc

--- src/main/java/com/github/davidmoten/reels/Actor.java
+++ src/main/java/com/github/davidmoten/reels/Actor.java
@@ -4,24 +4,27 @@ public interface Actor<T> {
 
     /**
      * Processes a queued message to the actor. If this method throws then the
-     * Supervisor allocated to the Actor will handle it (default is to write an
-     * error to stderr, clear the message queue and prevent further message
-     * processing).
+     * Supervisor allocated to the Actor will handle it (default is
+     * {@code Supervisor.defaultSupervisor()}.
      * 
-     * @param context gives access to self and sender {@link ActorRef}s and to
+     * @param context gives access to self (recipient) and sender {@link ActorRef}s and to
      *                overall {@link Context} (which might be used for creating more
      *                actors for example)
      * @param message message to be processed
      */
     void onMessage(Message<T> message);
-    
+
+    /**
+     * Called just after creating this, before processing any messages.
+     * @param self ActorRef referring to this
+     */
     void preStart(ActorRef<T> self);
 
     /**
      * This method called after an actor is stopped (via {@link ActorRef#stop()} or
      * when an actor is restarted.
      * 
-     * @param self actor ref for this
+     * @param self ActorRef referring to this
      */
     void onStop(ActorRef<T> self);
 
