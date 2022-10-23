96870b8cfa016b4ef98a1a098d754905ad8f93e4
Dave Moten <davidmoten@gmail.com>
2022-10-24 10:22:46 +1100

    add tests

--- src/test/java/com/github/davidmoten/reels/internal/queue/MpscLinkedQueueTest.java
+++ src/test/java/com/github/davidmoten/reels/internal/queue/MpscLinkedQueueTest.java
@@ -1,6 +1,7 @@
 package com.github.davidmoten.reels.internal.queue;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
 
 import org.junit.Test;
 
@@ -18,6 +19,20 @@ public class MpscLinkedQueueTest {
         q.offer(1, 2);
         assertEquals(1,  q.poll());
         assertEquals(2,  q.poll());
+    }
+    
+    @Test
+    public void testEmpty() {
+        MpscLinkedQueue<Object> q = new MpscLinkedQueue<>();
+        assertTrue(q.isEmpty());
+    }
+    
+    @Test
+    public void testPollThenEmpty() {
+        MpscLinkedQueue<Object> q = new MpscLinkedQueue<>();
+        q.offer(1);
+        q.clear();
+        assertTrue(q.isEmpty());
     }
 
 }
